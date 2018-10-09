8f6fa8adcbd563248ed134a92c6b2806e59b027f
吳駿松 <jo32229hn@gmail.com>
2018-10-09 22:18:24 +0800

    no message

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,15 @@
+<?php
+namespace Demo\Hello;
+class Someone
+{
+    function construct($name)
+    {
+        echo 'Hello'.$name.PHP_EOL;
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: Anriaru
+ * Date: 2018/10/9
+ * Time: 下午 10:16
+ */
