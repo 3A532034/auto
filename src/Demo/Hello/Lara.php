dc54b13d40e3f7f462a792be9d9627810e64ad12
吳駿松 <jo32229hn@gmail.com>
2018-10-09 22:15:51 +0800

    no message

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,15 @@
+<?php
+namespace Demo\Hello;
+class Lara
+{
+    function construct()
+    {
+        echo 'Hello Lara~!'.PHP_EOL;
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: Anriaru
+ * Date: 2018/10/9
+ * Time: 下午 10:14
+ */
