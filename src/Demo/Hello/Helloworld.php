faa3f1ec1fd4a501f5f445c0a6dbfd5a76f8c383
吳駿松 <jo32229hn@gmail.com>
2018-10-09 22:13:05 +0800

    no message

---
+++ src/Demo/Hello/Helloworld.php
@@ -0,0 +1,15 @@
+<?php
+namespace Demo;
+class Helloworld
+{
+    function construct()
+    {
+        echo 'Hello World~!'.PHP_EOL;
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: Anriaru
+ * Date: 2018/10/9
+ * Time: 下午 10:09
+ */
